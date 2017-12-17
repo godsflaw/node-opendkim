a92eec1c3b64a5e417948079d51bb802cc538b4b
Louis Pearson <desttinghimgame@gmail.com>
2017-12-17 01:00:00 -0700

    ISSUE-3: Make eom() chainable by default
    * Passing {testkey: true} to eom() uses its previous behavior
    * Added a test for chaining and for testkey

--- test/01-processing/03-eom.js
+++ test/01-processing/03-eom.js
@@ -45,3 +45,68 @@ test('test eom works after header, eoh, and body calls', t => {
     t.fail();
   }
 });
+
+test('test eom chaining', t => {
+  try {
+    var opendkim = new OpenDKIM();
+
+    opendkim.query_method('DKIM_QUERY_FILE');
+    opendkim.query_info('../fixtures/testkeys');
+
+    opendkim.verify({id: undefined});
+    var header = messages.good.substring(0, messages.good.indexOf('\r\n\r\n'));
+    var body = messages.good.substring(messages.good.indexOf('\r\n\r\n') + 4);
+    var headers = header.replace(/\r\n\t/g, ' ').split(/\r\n/);
+    for (var i = 0; i < headers.length; i++) {
+      var line = headers[i];
+      opendkim.header({
+        header: line,
+        length: line.length
+      });
+    }
+    opendkim.eoh();
+    opendkim.body({
+      body: body,
+      length: body.length
+    });
+    opendkim.eom().sig_getdomain();
+    t.pass();
+  } catch (err) {
+    console.log(err);
+    t.fail();
+  }
+});
+
+test('test eom testkey', t => {
+  try {
+    var opendkim = new OpenDKIM();
+
+    opendkim.query_method('DKIM_QUERY_FILE');
+    opendkim.query_info('../fixtures/testkeys');
+
+    opendkim.verify({id: undefined});
+    var header = messages.good.substring(0, messages.good.indexOf('\r\n\r\n'));
+    var body = messages.good.substring(messages.good.indexOf('\r\n\r\n') + 4);
+    var headers = header.replace(/\r\n\t/g, ' ').split(/\r\n/);
+    for (var i = 0; i < headers.length; i++) {
+      var line = headers[i];
+      opendkim.header({
+        header: line,
+        length: line.length
+      });
+    }
+    opendkim.eoh();
+    opendkim.body({
+      body: body,
+      length: body.length
+    });
+    var istest = opendkim.eom({testkey: true});
+    if (!istest) { // this is a test
+      t.fail();
+    }
+    t.pass();
+  } catch (err) {
+    console.log(err);
+    t.fail();
+  }
+});
