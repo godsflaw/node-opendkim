82c03c35aa8d8bd5d20b4722e73a6212cf9c65e7
Christopher Mooney <chris@dod.net>
2017-12-21 14:08:42 -0500

    ISSUE-63: add test data and basic test for message with z=

---
+++ test/04-verifying/31-message-good-z.js
@@ -0,0 +1,54 @@
+import test from 'ava';
+
+var OpenDKIM = require('../../');
+var Messages = require('../fixtures/messages');
+
+var messages = new Messages();
+
+test('test good message with z= chunk', t => {
+  try {
+    var opendkim = new OpenDKIM();
+
+    opendkim.query_method('DKIM_QUERY_FILE');
+    opendkim.query_info('../fixtures/testkeys');
+
+    opendkim.verify({id: undefined});
+    opendkim.chunk({
+      message: messages.good_z,
+      length: messages.good_z.length
+    });
+    opendkim.chunk_end();
+    t.pass();
+  } catch (err) {
+    console.log(err);
+    t.fail();
+  }
+});
+
+test('test good message with z= multi-chunk', t => {
+  try {
+    var opendkim = new OpenDKIM();
+
+    opendkim.query_method('DKIM_QUERY_FILE');
+    opendkim.query_info('../fixtures/testkeys');
+
+    var chunks = 16;
+    var numChunks = Math.ceil(messages.good_z.length / chunks);
+
+    opendkim.verify({id: undefined});
+
+    for (var i = 0, o = 0; i < numChunks; ++i, o += chunks) {
+      var chunk = messages.good_z.substr(o, chunks);
+      opendkim.chunk({
+        message: chunk,
+        length: chunk.length
+      });
+    }
+
+    opendkim.chunk_end();
+    t.pass();
+  } catch (err) {
+    console.log(err);
+    t.fail();
+  }
+});
